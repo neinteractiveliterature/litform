0ab4f73dcd924f3273d62964a7afe016765b4f6c
Nat Budin <natbudin@gmail.com>
2021-05-17 12:51:30 -0700

    Set up Storybook

---
+++ src/stories/BootstrapFormInput.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, Story } from '@storybook/react';
+import BootstrapFormInput, { BootstrapFormInputProps } from '../BootstrapFormInput';
+
+export default {
+  title: 'Forms/BootstrapFormInput',
+  component: BootstrapFormInput,
+  argTypes: {
+    label: { control: 'text' },
+    hideLabel: { control: 'boolean' },
+    helpText: { control: 'text' },
+    invalidFeedback: { control: 'text' },
+  },
+} as Meta;
+
+const Template: Story<BootstrapFormInputProps> = (args) => <BootstrapFormInput {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = {
+  label: 'First name',
+};
+
+export const Invalid = Template.bind({});
+Invalid.args = {
+  label: 'Email address',
+  type: 'email',
+  value: 'not-valid@@gmail.com',
+  className: 'form-control is-invalid',
+  invalidFeedback: 'Please enter a valid email address.',
+};
