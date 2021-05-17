f8498ec817a64559a99f4cafd88da78dfcc5c7bd
Nat Budin <natbudin@gmail.com>
2021-05-17 12:21:13 -0700

    Pull in a lot of Intercode's basic form elements

---
+++ src/BootstrapFormInput.tsx
@@ -0,0 +1,68 @@
+import { InputHTMLAttributes, ReactNode } from 'react';
+import * as React from 'react';
+import useUniqueId from './useUniqueId';
+import HelpText from './HelpText';
+
+type BootstrapFormInputPropsCommon = InputHTMLAttributes<HTMLInputElement> & {
+  label: ReactNode;
+  hideLabel?: boolean;
+  helpText?: ReactNode;
+  invalidFeedback?: ReactNode;
+};
+
+type BootstrapFormInputPropsWithHTMLChange = BootstrapFormInputPropsCommon;
+type BootstrapFormInputPropsWithTextChange = Omit<BootstrapFormInputPropsCommon, 'onChange'> & {
+  onTextChange: (text: string) => void;
+};
+
+export type BootstrapFormInputProps =
+  | BootstrapFormInputPropsWithHTMLChange
+  | BootstrapFormInputPropsWithTextChange;
+
+function isHTMLChangeProps(
+  props: BootstrapFormInputProps,
+): props is BootstrapFormInputPropsWithHTMLChange {
+  return !Object.prototype.hasOwnProperty.call(props, 'onTextChange');
+}
+
+function extractInputElementAttributes(
+  props: BootstrapFormInputProps,
+): InputHTMLAttributes<HTMLInputElement> {
+  if (isHTMLChangeProps(props)) {
+    const { helpText, label, hideLabel, invalidFeedback, type, onChange, ...otherProps } = props;
+    return otherProps;
+  }
+
+  const { helpText, label, hideLabel, invalidFeedback, type, onTextChange, ...otherProps } = props;
+  return otherProps;
+}
+
+function BootstrapFormInput(props: BootstrapFormInputProps) {
+  const inputId = useUniqueId(`${props.name || 'input'}-`);
+  const inputAttributes = extractInputElementAttributes(props);
+
+  const onChangeProp = isHTMLChangeProps(props)
+    ? props.onChange
+    : (event: React.ChangeEvent<HTMLInputElement>) => {
+        props.onTextChange(event.target.value);
+      };
+
+  return (
+    <div className="mb-3">
+      <label htmlFor={inputId} className={props.hideLabel ? 'sr-only' : undefined}>
+        {props.label}
+      </label>
+      <input
+        className="form-control"
+        id={inputId}
+        onChange={onChangeProp}
+        type={props.type ?? 'text'}
+        {...inputAttributes}
+      />
+      <HelpText>{props.helpText}</HelpText>
+      {props.invalidFeedback && <div className="invalid-feedback">{props.invalidFeedback}</div>}
+    </div>
+  );
+}
+
+export default BootstrapFormInput;
---
+++ src/FormGroupWithLabel.tsx
@@ -0,0 +1,36 @@
+import { ReactNode } from 'react';
+
+import useUniqueId from './useUniqueId';
+import HelpText from './HelpText';
+
+export type FormGroupWithLabelProps = {
+  children: (id: string) => ReactNode;
+  label: ReactNode;
+  name?: string;
+  helpText?: ReactNode;
+  className?: string;
+  labelClassName?: string;
+};
+
+function FormGroupWithLabel({
+  children,
+  label,
+  name,
+  helpText,
+  className,
+  labelClassName,
+}: FormGroupWithLabelProps) {
+  const id = useUniqueId(`${name || 'input'}-`);
+
+  return (
+    <div className={className ?? 'mb-3'}>
+      <label htmlFor={id} className={labelClassName}>
+        {label}
+      </label>
+      {children(id)}
+      <HelpText>{helpText}</HelpText>
+    </div>
+  );
+}
+
+export default FormGroupWithLabel;
