a0d20bd7a9134c242d9fd844f239cec0df899dfb
Nat Budin <natbudin@gmail.com>
2021-06-06 12:29:45 -0700

    Add missing dev dependencies; upgrade react-bootstrap4-modal

--- src/Alert.tsx
+++ src/Alert.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, ReactNode } from 'react';
-import Modal from 'react-bootstrap4-modal';
+import { Modal } from 'react-bootstrap4-modal';
 
 import useModal from './useModal';
 
