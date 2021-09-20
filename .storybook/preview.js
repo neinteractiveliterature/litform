16634183fe0299e0f4c596d2a2226926a2f0afb6
Nat Budin <natbudin@gmail.com>
2021-09-20 11:13:31 -0700

    Custom sorting; add an intro to how forms work in Litform

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -22,4 +22,33 @@ export const parameters = {
       state: true,
     },
   },
+  options: {
+    storySort: (a, b) => {
+      const aSplit = a[1].kind.split('/');
+      const bSplit = b[1].kind.split('/');
+      const aPath = aSplit.slice(0, aSplit.length - 1).join('/');
+      const bPath = bSplit.slice(0, bSplit.length - 1).join('/');
+
+      if (aPath !== bPath) {
+        return aPath.localeCompare(bPath, undefined, { sensitivity: 'base' });
+      }
+
+      if (a[1].name === 'Page' && b[1].name !== 'Page') {
+        return -1;
+      }
+
+      if (a[1].name !== 'Page' && b[1].name === 'Page') {
+        return 1;
+      }
+
+      const aSlashCount = (a[1].kind.match(/\//g) || []).length;
+      const bSlashCount = (b[1].kind.match(/\//g) || []).length;
+
+      if (aSlashCount === bSlashCount) {
+        return a[1].kind.localeCompare(b[1].kind, undefined, { sensitivity: 'base' });
+      }
+
+      return aSlashCount - bSlashCount;
+    },
+  },
 };
